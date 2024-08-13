5ae4c85547d55f53633c0414e1c9227691038e7b
francescazfl <francesca.zf.l@berkeley.edu>
2024-08-12 17:34:42 -0700

    fix tweetie or not

--- execute_analysis.py
+++ execute_analysis.py
@@ -7,7 +7,8 @@ from __future__ import annotations
 from src.analysis import parse_results
 
 if __name__ == "__main__":
-    parse_results("results/all_*")
-    parse_results(["results/4eq_36", "results/4eq_48", "results/4eq_72", "results/4eq_96", "results/4eq_120"])
-    parse_results(["results/3eq_40", "results/3eq_96", "results/3eq_128"])
-    parse_results(["results/2eq_60", "results/2eq_72", "results/2eq_96", "results/2eq_144", "results/2eq_240"])
+    parse_results(["results/4eq_168", "results/4eq_264", "results/4eq_504", "results/3eq_224", "results/3eq_352", "results/3eq_672", "results/2eq_1008"])
+    # parse_results("results/all_*")
+    # parse_results(["results/4eq_36", "results/4eq_48", "results/4eq_72", "results/4eq_96", "results/4eq_120", "results/4eq_144"])
+    # parse_results(["results/3eq_40", "results/3eq_48", "results/3eq_64", "results/3eq_96", "results/3eq_128", "results/3eq_192"])
+    # parse_results(["results/2eq_60", "results/2eq_72", "results/2eq_96", "results/2eq_144", "results/2eq_192", "results/2eq_240", "results/2eq_288", "results/2eq_336", "results/2eq_528"])
--- src/analysis.py
+++ src/analysis.py
@@ -23,27 +23,32 @@ def index2regret(indices, y):
     regret = torch.zeros((indices.shape[0], indices.shape[1]))
     for i in range(indices.shape[0]):
         for j in range(indices.shape[1]):
-            regret[i, j] = 1 - y[indices[i, :j+1]].max()
+            regret[i, j] = 1 - y[indices[i, : j + 1]].max()
     return regret
+
 
 def load_tensors(subdir, y):
     """
     loads al indices from a directory and converts them to regret values
     """
-    tensors = sorted(glob.glob(subdir + '/*indices*.pt'))
-        
+    tensors = sorted(glob.glob(subdir + "/*indices*.pt"))
+
     tests = {}
-    print('Models not included/not over budget yet:\n')
+    print("Models not included/not over budget yet:\n")
 
     for tensor in tensors:
-        num = int(tensor.split('indices')[0].split('_')[-1])
+        num = int(tensor.split("indices")[0].split("_")[-1])
 
-        if '.pt' in tensor and 'state_dict' not in tensor:
+        if ".pt" in tensor and "state_dict" not in tensor:
             first = False
             if "Random" not in os.path.basename(tensor):
-                nm = os.path.basename(tensor).split('_')[0] + '_' + os.path.basename(tensor).split('_')[1]
-            else:     
-                nm = os.path.basename(tensor).split('_')[0]
+                nm = (
+                    os.path.basename(tensor).split("_")[0]
+                    + "_"
+                    + os.path.basename(tensor).split("_")[1]
+                )
+            else:
+                nm = os.path.basename(tensor).split("_")[0]
 
             t = torch.load(tensor).cpu().detach()
             t = torch.reshape(t, (1, -1))
@@ -59,8 +64,8 @@ def load_tensors(subdir, y):
                 d = {}
                 tests[nm] = d
 
-            dtype = os.path.basename(tensor).split('_')[-1].split('.')[0]
-            dtype = ''.join([i for i in dtype if not i.isdigit()])
+            dtype = os.path.basename(tensor).split("_")[-1].split(".")[0]
+            dtype = "".join([i for i in dtype if not i.isdigit()])
             if dtype in d.keys():
                 arr = d[dtype]
             else:
@@ -70,35 +75,36 @@ def load_tensors(subdir, y):
             if first:
                 pass
             elif t.size(-1) < arr.size(-1):
-                arr = torch.cat((arr[:,:t.size(-1)], t), 0)
+                arr = torch.cat((arr[:, : t.size(-1)], t), 0)
             else:
-                arr = torch.cat((arr, t[:,:arr.size(-1)]), 0)
+                arr = torch.cat((arr, t[:, : arr.size(-1)]), 0)
             d[dtype] = arr
             tests[nm] = d
 
-    print('\nModels included:\n')
+    print("\nModels included:\n")
     batch = {}
-    budget, total= math.inf, math.inf
+    budget, total = math.inf, math.inf
     for key in tests.keys():
         # print(key)
         num_runs = -1
         for dtype in tests[key].keys():
             t = tests[key][dtype]
             t = index2regret(t, y)
-            
-            if 'indices' == dtype and t.size(-1) != 0:
+
+            if "indices" == dtype and t.size(-1) != 0:
                 if t.size(-1) < budget:
                     budget = t.size(-1)
                 num_runs = t.size(0)
-            elif 'y' in dtype and t.size(-1) < total and t.size(-1) != 0:
+            elif "y" in dtype and t.size(-1) < total and t.size(-1) != 0:
                 total = t.size(-1)
-            
+
             sd, mean = torch.std_mean(t, 0)
-            sem = sd / (t.size(0)**.5)
+            sem = sd / (t.size(0) ** 0.5)
             # add fraction achieve max regret
-            frac_max_mean = torch.where(t == 0, torch.tensor(1.), torch.tensor(0.)).sum(dim=0) / t.size(0)
-        
-            
+            frac_max_mean = torch.where(
+                t == 0, torch.tensor(1.0), torch.tensor(0.0)
+            ).sum(dim=0) / t.size(0)
+
             if dtype in batch.keys():
                 d = batch[dtype]
             else:
@@ -110,10 +116,11 @@ def load_tensors(subdir, y):
             batch[dtype] = d
         print("Runs: {}".format(num_runs))
     print(batch.keys())
-    print('Budget: {}'.format(budget))
-    print('Total queries (incl. init): {}'.format(budget))
+    print("Budget: {}".format(budget))
+    print("Total queries (incl. init): {}".format(budget))
 
     return batch, budget
+
 
 def tabulate_regret(df, tests, budget, subdir, randregret=None):
     """
@@ -121,7 +128,7 @@ def tabulate_regret(df, tests, budget, subdir, randregret=None):
     """
     names = []
     queries = np.arange(budget) + 1
-    
+
     for name in sorted(tests.keys()):
         names.append(name)
 
@@ -134,21 +141,33 @@ def tabulate_regret(df, tests, budget, subdir, randregret=None):
             mean = mean[:budget]
             sem = sem[:budget]
 
-        protein = subdir.split('/')[-2]
-        encoding = subdir.split('/')[-1]
+        protein = subdir.split("/")[-2]
+        encoding = subdir.split("/")[-1]
 
-        if 'Random' in name:
-            encoding = 'Random'
-            model = 'Random'
-            acquisition = 'Random'
+        if "Random" in name:
+            encoding = "Random"
+            model = "Random"
+            acquisition = "Random"
         else:
-            names = name.split('-')
+            names = name.split("-")
             model = names[0]
             acquisition = names[-2]
 
-        for timestep, single_mean, single_std, single_frac_max in zip(queries, np.array(mean), np.array(sem), np.array(frac_max)):
-            df.loc[len(df.index)] = [protein, encoding, model, acquisition, timestep, single_mean, single_std, single_frac_max]
+        for timestep, single_mean, single_std, single_frac_max in zip(
+            queries, np.array(mean), np.array(sem), np.array(frac_max)
+        ):
+            df.loc[len(df.index)] = [
+                protein,
+                encoding,
+                model,
+                acquisition,
+                timestep,
+                single_mean,
+                single_std,
+                single_frac_max,
+            ]
     return df
+
 
 def parse_results(res_dirs: str | list[str]):
     """
@@ -162,33 +181,64 @@ def parse_results(res_dirs: str | list[str]):
     else:
         res_dir_list = deepcopy(res_dirs)
 
-    #Loop over different datasets and encodings
+    # Loop over different datasets and encodings
     for res_dir in res_dir_list:
 
         print(f"Processing {res_dir}...")
-        #tabulate all max fitness values into one dataframe
-        df = pd.DataFrame(columns=['Protein', 'Encoding', 'Model', 'Acquisition', 'Timestep', 'Mean', 'Std', "Frac"])
+        # tabulate all max fitness values into one dataframe
+        df = pd.DataFrame(
+            columns=[
+                "Protein",
+                "Encoding",
+                "Model",
+                "Acquisition",
+                "Timestep",
+                "Mean",
+                "Std",
+                "Frac",
+            ]
+        )
 
-        for protein in ["DHFR", "GB1", "ParD2", "ParD3", "TrpB3A", "TrpB3B", "TrpB3C", "TrpB3D", "TrpB3E", "TrpB3F", "TrpB3G", "TrpB3H", "TrpB3I", "TrpB4"]:
-            for encoding in ['onehot']:
-                subdir =  res_dir + '/' + protein + '/' + encoding
-                fitness_df = pd.read_csv('data/' + protein + '/fitness.csv')
-                y = fitness_df['fitness'].values
-                y = y/y.max()
+        for protein in [
+            "DHFR",
+            "GB1",
+            "ParD2",
+            "ParD3",
+            "TrpB3A",
+            "TrpB3B",
+            "TrpB3C",
+            "TrpB3D",
+            "TrpB3E",
+            "TrpB3F",
+            "TrpB3G",
+            "TrpB3H",
+            "TrpB3I",
+            "TrpB4",
+        ]:
+            for encoding in ["onehot"]:
+                subdir = res_dir + "/" + protein + "/" + encoding
+                fitness_df = pd.read_csv("data/" + protein + "/fitness.csv")
+                y = fitness_df["fitness"].values
+                y = y / y.max()
 
                 print(subdir)
-            
-                batch, budget = load_tensors(subdir, y)
-                
-                df = tabulate_regret(df, batch['indices'], budget, subdir, randregret=None)
 
-        #clean up the results and save
-        df = df.drop_duplicates(subset=['Protein', 'Encoding', 'Model', 'Acquisition', 'Timestep'], keep='first')
-        df['Model'] = df['Model'].replace('BOOSTING_ENSEMBLE', 'Boosting Ensemble')
-        df['Model'] = df['Model'].replace('GP_BOTORCH', 'GP')
-        df['Model'] = df['Model'].replace('DNN_ENSEMBLE', 'DNN Ensemble')
-        df['Model'] = df['Model'].replace('DKL_BOTORCH', 'DKL')
-        df['Acquisition'] = df['Acquisition'].replace('Random', 'GREEDY')
-        df.to_csv(res_dir + '/all_results.csv', index=False)
+                batch, budget = load_tensors(subdir, y)
+
+                df = tabulate_regret(
+                    df, batch["indices"], budget, subdir, randregret=None
+                )
+
+        # clean up the results and save
+        df = df.drop_duplicates(
+            subset=["Protein", "Encoding", "Model", "Acquisition", "Timestep"],
+            keep="first",
+        )
+        df["Model"] = df["Model"].replace("BOOSTING_ENSEMBLE", "Boosting Ensemble")
+        df["Model"] = df["Model"].replace("GP_BOTORCH", "GP")
+        df["Model"] = df["Model"].replace("DNN_ENSEMBLE", "DNN Ensemble")
+        df["Model"] = df["Model"].replace("DKL_BOTORCH", "DKL")
+        df["Acquisition"] = df["Acquisition"].replace("Random", "GREEDY")
+        df.to_csv(res_dir + "/all_results.csv", index=False)
 
         del df
