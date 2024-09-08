d127cdd302f2f187b5a6d7f90bc0d225f234024c
francescazfl <francesca.zf.l@berkeley.edu>
2024-09-08 09:56:41 -0700

    run all zs analysis

--- execute_analysis.py
+++ execute_analysis.py
@@ -7,12 +7,12 @@ from __future__ import annotations
 from src.analysis import parse_results
 
 if __name__ == "__main__":
-    parse_results(["results/coves_4eq_30", "results/coves_4eq_36",  "results/coves_4eq_48", "results/coves_4eq_72", "results/coves_4eq_96", "results/coves_4eq_120", "results/coves_4eq_144"])
-    parse_results(["results/ed_4eq_36", "results/ed_4eq_48", "results/ed_4eq_72", "results/ed_4eq_96", "results/ed_4eq_120", "results/ed_4eq_144", "results/ed_4eq_168"])
-    parse_results(["results/esm_4eq_36", "results/esm_4eq_48", "results/esm_4eq_96", "results/esm_4eq_120"])
-    parse_results(["results/esmif_4eq_72", "results/esmif_4eq_96", "results/esmif_4eq_120", "results/esmif_4eq_144", "results/esmif_4eq_168", "results/esmif_4eq_264"])
-    parse_results(["results/ev_4eq_36", "results/ev_4eq_72", "results/ev_4eq_96", "results/ev_4eq_120", "results/ev_4eq_144", "results/ev_4eq_168", "results/ev_4eq_264"])
-    parse_results(["results/Triad_4eq_72", "results/Triad_4eq_96", "results/Triad_4eq_120"])
+    # parse_results("results/coves_*")
+    # parse_results("results/esmif_*")
+    # parse_results("results/ev_*")
+    # parse_results("results/ed_*")
+    parse_results("results/Triad_*")
+    parse_results("results/esm_*")
     # parse_results("results/all_*")
     # parse_results("results/4eq_*")
     # parse_results("results/3eq_*")
